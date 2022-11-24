d635a2000219b977321db56d7af796b92f1b143f
easydoesit <michael.grills@gmail.com>
2022-11-24 04:42:07 +0000

    add urls_show template

--- express_server.js
+++ express_server.js
@@ -26,6 +26,13 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  //console.log(req);
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  console.log(templateVars);
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
