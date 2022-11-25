1c90b4a2049b07ac5b73dee2527edb270f48ce43
easydoesit <michael.grills@gmail.com>
2022-11-25 03:04:28 +0000

    add check for error on id and long url

--- express_server.js
+++ express_server.js
@@ -2,15 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+// use ejs
 app.set("view engine", "ejs");
+
+// parse data in buffer to make it readable.
+app.use(express.urlencoded({ extended: true }));
+
+
+////////////////////////////////////////////////////
+// Database
+////////////////////////////////////////////////////
 
 const urlDatabase = {
   "b2xVn2" : "http://www.lighthouselabs.ca",
   "9sm5xK" : "http://www.google.com"
 };
 
-// parse data in buffer to make it readable.
-app.use(express.urlencoded({ extended: true }));
+////////////////////////////////////////////////////
+// Routes
+////////////////////////////////////////////////////
 
 app.get('/', (req, res) => {
   res.send("Hello!");
@@ -24,37 +34,58 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// All Urls List
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase};
   res.render('urls_index', templateVars);
 });
 
+// New URL Form SHOW
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
-});
-
-app.get("/u/:id", (req, res) => {
-  const longURL = (urlDatabase[req.params.id]);
-  res.redirect(longURL);
-});
-
+// Submit new URL
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  //app.use(express.bodyParser());
+  console.log(req.body);
+
   const shortUrl = generateRandomString(6);
   urlDatabase[shortUrl] = req.body.longURL;
+  
+  res.redirect(`/urls/${shortUrl}`);
 
-  res.redirect(`/urls/${shortUrl}`); // Respond with 'Ok' (we will replace this)
 });
+
+// Show Url by ID
+app.get("/urls/:id", (req, res) => {
+  if (!urlDatabase[req.params.id]) {
+    res.render('urls_noID');
+  } else {
+    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+    res.render("urls_show", templateVars);
+  }
+});
+
+// Redirect to Long URL
+app.get("/u/:id", (req, res) => {
+  const longURL = (urlDatabase[req.params.id]);
+  if (longURL === undefined) {
+    res.render('urls_noID');
+  } else {
+    res.redirect(longURL);
+  }
+});
+
+
+////////////////////////////////////////////////////
+// Server Listening...
+////////////////////////////////////////////////////
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+// Functions
 
 const generateRandomString = function(size) {
   let result = "";
