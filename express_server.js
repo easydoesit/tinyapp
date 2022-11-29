717dbb14ac240f9efec3cb8b8720d06d82543f6f
easydoesit <michael.grills@gmail.com>
2022-11-29 07:46:36 +0000

    added registration page

--- express_server.js
+++ express_server.js
@@ -3,21 +3,31 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-// file-system Setup
-const fs = require('fs');
-
-////////////////////////////////////////////////////
-// Database
-////////////////////////////////////////////////////
-
-const urlDatabase = require("./data/urlDataBase.json");
-//console.log(urlDatabase);
-
 // use ejs
 app.set("view engine", "ejs");
 
 // parse data in buffer to make it readable.
 app.use(express.urlencoded({ extended: true }));
+
+// file-system Setup
+const fs = require('fs');
+
+////////////////////////////////////////////////////
+// Databases
+////////////////////////////////////////////////////
+
+const urlDatabase = require("./data/urlDataBase.json");
+const users = require("./data/users.json");
+//console.log(urlDatabase);
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/register.json", (req, res) => {
+  res.json(users);
+});
+
 
 ////////////////////////////////////////////////////
 // Cookies
@@ -34,20 +44,44 @@ app.get('/', (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// register
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (email === "" || password === "") {
+    console.log("empty string");
+    res.send("Error: 400. You must include a name and password.");
+  } else if (userLookUpByEmail(email)) {
+    console.log("email exists");
+    res.send("Error: 400. That email already exists");
+  } else {
+    const userID = generateRandomString(6);
+    users[userID] = {id: userID, email : email, password : password};
+    writeToFile('./data/users.json', users);
+    res.cookie('userID', userID);
+    res.redirect(301, '/urls');
+  }
+});
 
 // Sign in
+
+app.get("/login", (req, res) =>{
+
+});
+
 app.post("/login", (req, res) => {
   console.log(req.body);
 
-  res.cookie('username', req.body);
+  res.cookie('userID', req.body);
 
   res.redirect(303, `/urls`);
 
@@ -56,7 +90,7 @@ app.post("/login", (req, res) => {
 // Sign Out
 app.post("/logout", (req, res) => {
   console.log(req.body);
-  res.clearCookie('username', req.body);
+  res.clearCookie('userID', req.body);
 
   res.redirect(303, `/urls`);
 
@@ -66,16 +100,17 @@ app.post("/logout", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user: users[req.cookies["userID"]]
   };
-
+  console.log(templateVars);
   res.render('urls_index', templateVars);
 
 });
 
 // New URL Form SHOW
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"]};
+  const templateVars = { user: users[req.cookies["userID"]]};
+  console.log(templateVars);
   res.render("urls_new", templateVars);
 });
 
@@ -101,7 +136,7 @@ app.post("/urls/:id", (req, res) => {
   urlDatabase[shortUrl] = req.body.longURL;
 
   writeToFile('./data/urlDataBase.json', urlDatabase);
-
+  
   res.redirect(303, `/urls/${shortUrl}`);
 
 });
@@ -118,9 +153,10 @@ app.post(`/urls/:id/delete`, (req, res) => {
 // Show Url by ID
 app.get("/urls/:id", (req, res) => {
   if (!urlDatabase[req.params.id]) {
-    res.render('urls_noID');
+    const templateVars = { user: users[req.cookies["userID"]]};
+    res.render('urls_noID', templateVars);
   } else {
-    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: users[req.cookies["userID"]] };
     res.render("urls_show", templateVars);
   }
 });
@@ -128,8 +164,9 @@ app.get("/urls/:id", (req, res) => {
 // Redirect to Long URL
 app.get("/u/:id", (req, res) => {
   const longURL = (urlDatabase[req.params.id]);
+  const templateVars = { user: users[req.cookies["userID"]]};
   if (longURL === undefined) {
-    res.render('urls_noID');
+    res.render('urls_noID', templateVars);
   } else {
     res.redirect(301, longURL);
   }
@@ -168,6 +205,14 @@ const writeToFile = function(file, body) {
     }
   });
 
+};
+
+// look up user by email
+const userLookUpByEmail = function(email) {
+  for (let key of Object.keys(users))
+    if (users[key].email === email) {
+      return true;
+    } else return false;
 };
 
 
