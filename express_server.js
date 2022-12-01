2cf41a3409d80a5ef5c2c3c2312e93b9cc8e0525
easydoesit <michael.grills@gmail.com>
2022-12-01 05:25:27 +0000

    move all helper functions to helpers.js

--- express_server.js
+++ express_server.js
@@ -20,7 +20,7 @@ const morgan = require("morgan");
 app.use(morgan('dev'));
 
 // helper functons
-const { userLookUpByEmail } = require('./helpers');
+const { userLookUpByEmail, writeToFile, userCheckPassword, urlsForUser, generateRandomString } = require('./helpers');
 
 ////////////////////////////////////////////////////
 // Databases
@@ -287,61 +287,3 @@ app.listen(PORT, () => {
 
 // Functions
 
-const generateRandomString = function(size) {
-  let result = "";
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-
-  for (let i = 0; i < size; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-
-  return result;
-
-};
-
-// write to file takes a file and the info you want to write. TODO - Make it write one line at a time.
-const writeToFile = function(file, body) {
-  const jsonString = JSON.stringify(body);
-
-  fs.writeFile(file, jsonString, err => {
-    if (err) {
-      console.log('Error writing file', err);
-    } else {
-      console.log('Successfully wrote file');
-    }
-
-  });
-};
-
-
-// check user password
-const userCheckPassword = function(password) {
-  let passwordCheck = false;
-
-  for (let key of Object.keys(users)) {
-    if  (bcrypt.compareSync(password, users[key].password)) {
-      passwordCheck = true;
-    }
-  
-  }
-
-  return passwordCheck;
-
-};
-
-// make an object of the URLS for a user.
-const urlsForUser = function(userID) {
-  const usersURLs = {};
-
-  for (let key of Object.keys(urlDatabase)) {
-    
-    if (urlDatabase[key].userID === userID) {
-      usersURLs[key] = {};
-      usersURLs[key].longURL = urlDatabase[key].longURL;
-    }
-
-  }
-
-  return usersURLs;
-};
--- helpers.js
+++ helpers.js
@@ -14,4 +14,64 @@ const userLookUpByEmail = function(email, database) {
 
 };
 
-module.exports = { userLookUpByEmail };
+// write to file takes a file and the info you want to write. TODO - Make it write one line at a time.
+const writeToFile = function(file, body) {
+  const jsonString = JSON.stringify(body);
+
+  fs.writeFile(file, jsonString, err => {
+    if (err) {
+      console.log('Error writing file', err);
+    } else {
+      console.log('Successfully wrote file');
+    }
+
+  });
+};
+
+// make an object of the URLS for a user.
+const urlsForUser = function(userID) {
+  const usersURLs = {};
+
+  for (let key of Object.keys(urlDatabase)) {
+    
+    if (urlDatabase[key].userID === userID) {
+      usersURLs[key] = {};
+      usersURLs[key].longURL = urlDatabase[key].longURL;
+    }
+
+  }
+
+  return usersURLs;
+};
+
+// check user password
+const userCheckPassword = function(password) {
+  let passwordCheck = false;
+
+  for (let key of Object.keys(users)) {
+    if  (bcrypt.compareSync(password, users[key].password)) {
+      passwordCheck = true;
+    }
+  
+  }
+
+  return passwordCheck;
+
+};
+
+// generate a random string
+const generateRandomString = function(size) {
+  let result = "";
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < size; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+
+};
+
+
+module.exports = { userLookUpByEmail, writeToFile, urlsForUser, userCheckPassword, generateRandomString };
